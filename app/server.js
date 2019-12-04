e1c368fcc67fd06cf00945797492b4762076e95a
juri <juri@paybase.io>
2019-12-04 16:54:58 +0000

    fix(webhook): improved output

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ const ANCHORE_USER = process.env.ANCHORE_USER || 'admin';
 const ANCHORE_PASS = process.env.ANCHORE_PASS || 'foobar';
 const ANCHORE_HOST = process.env.ANCHORE_HOST || 'localhost';
 const ANCHORE_PORT = process.env.ANCHORE_PORT || 8228;
-const SLACK_URL = process.env.SLACK_WEBHOOK_URL
+const SLACK_URL = process.env.SLACK_WEBHOOK_URL;
 
 const app = new Koa();
 const router = new Router();
@@ -31,8 +31,6 @@ app.use(async (ctx, next) => {
   console.log(`${new Date(n).toISOString()} - ${Date.now() - n}ms - ${ctx.method} ${ctx.url}`);
 });
 
-
-
 // response
 
 router.post('/analysis_update', async (ctx) => {
@@ -42,18 +40,16 @@ router.post('/analysis_update', async (ctx) => {
   const result = await request_imageId(image);
   const severe = result.filter(x => ['High', 'Critical'].includes(x.severity));
   console.log('length of severe vulns', severe.length);
-  const groups = severe.reduce((acc, { vuln, package, feed_group, fix, severity }) => {
+  const groups = severe.reduce((acc, { vuln, package, feed_group, fix, severity, url }) => {
     acc[package] = acc[package] || { feed_group, vulnerabilities: [] };
-    acc[package].vulnerabilities.push({ severity, vuln, fix });
+    acc[package].vulnerabilities.push({ severity, vuln, fix, url });
     return acc;
   }, {});
-  console.dir(groups, { depth: null });
-  var formatted_text = Object.entries(groups).map(([ package, { feed_group, vulnerabilities } ]) => {
-    return `${package} (${feed_group}) count: ${vulnerabilities.length} list: ${vulnerabilities.map(x => `- ${x.severity} (${x.vuln}) -> fix: ${x.fix} `).join(', ')}`
-  });
-  //console.log(formatted_text);
+  //var formatted_text = Object.entries(groups).map(([ package, { feed_group, vulnerabilities } ]) => {
+  //  return `${package} (${feed_group}) count: ${vulnerabilities.length} list: ${vulnerabilities.map(x => `- ${x.severity} (${x.vuln}) -> fix: ${x.fix} `).join(', ')}`
+  //});
   if (SLACK_URL){
-    await slack_notification(image, formatted_text);
+    await slack_notification(image, groups);
   }
   ctx.body = result;
 });
@@ -86,30 +82,59 @@ async function request_vuln(image, imageId) {
   return json.vulnerabilities;
 }
 
+const groupsToAttachments = groups =>
+  Object.entries(groups)
+    .map(([ package, { feed_group, vulnerabilities }]) => {
+      let highestSeverity = 'High';
+      const obj = {
+        title: `${package} (${feed_group})`,
+        fields: vulnerabilities.map(x => {
+          if (x.severity === 'Critical' && highestSeverity === 'High') highestSeverity = x.severity;
+          return {
+            title: x.vuln,
+            value: "fix: " + x.fix,
+          }
+        }),
+      };
+      obj.color = highestSeverity === 'Critical' ? '#E01E5A' : '#ECB22E';
+      return obj;
+    });
+
 // Send the notification
 
-async function slack_notification(image, text) {
+async function slack_notification(image, groups) {
   const slack_webhook = new IncomingWebhook(SLACK_URL);
+  const attachments = groupsToAttachments(groups);
+
   await slack_webhook.send({
-    username: 'Anchore vulnerabilities alert', // This will appear as user name who posts the message
-    text: JSON.stringify(text, null, "\t"), // text
-    icon_emoji: ':warning:', // User icon, you can also use custom icons here
-    attachments: [{ // this defines the attachment block, allows for better layout usage
-      color: '#eed140', // color of the attachments sidebar.
-      fields: [ // actual fields
-        {
-          title: 'Environment', // Custom field
-          value: 'QA', // Custom value
-          short: true // long fields will be full width
-        },
-        {
-          title: 'image', // Custom field
-          value: image, // Custom value
-          short: true // long fields will be full width
-        }
-      ]
-    }]
-    });
+    username: 'Anchore',
+    icon_emoji: ':warning:',
+    attachments: [
+      {
+        color: '#36C5F0',
+        title: "CVE vulnerabilities",
+        title_link: "https://cve.mitre.org/",
+        fields: [
+          {
+            title: 'Environment',
+            value: 'QA',
+            short: true
+          }
+        ]
+      },
+      {
+        color: '#2EB67D',
+        fields: [
+          {
+            title: 'image',
+            value: image,
+            short: false
+          }
+        ]
+      },
+      ...attachments,
+    ]
+  });
 }
 
 app.use(bodyParser());
